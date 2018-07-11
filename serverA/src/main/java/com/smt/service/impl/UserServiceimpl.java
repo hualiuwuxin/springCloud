7f154e5af666767dc42606ba89bf19d9499d1fe1
hualiuwuxin <451740146@qq.com>
2018-07-11 18:06:04 +0800

    啊

--- serverA/src/main/java/com/smt/service/impl/UserServiceimpl.java
+++ serverA/src/main/java/com/smt/service/impl/UserServiceimpl.java
@@ -1,16 +1,23 @@
 package com.smt.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.smt.bean.User;
 import com.smt.service.UserService;
+import com.smt.service.dao.UserMapper;
 
 @Service
 public class UserServiceimpl implements UserService {
-
+	
+	@Autowired
+	UserMapper userMapper;
+	
 	@Override
 	public User getUserById(long l) {
-		User user = new User();
+		
+		
+		User user = userMapper.selectByPrimaryKey( 1L );
 		user.setId(1L);
 		user.setName("admin");
 		return user;
